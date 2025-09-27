1df1c24b0a42bf3dcc0c0c449a9400950649c50e
Mohd Ashraf <98030979+mohdashraf1234@users.noreply.github.com>
2025-09-27 21:27:31 +0530

    Add files via upload

---
+++ EndUser/EndUser/src/main/java/com/enduse/EndUser/config/KafkaConfig.java
@@ -0,0 +1,23 @@
+package com.enduse.EndUser.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class KafkaConfig {
+	
+
+	
+	
+	
+
+	@KafkaListener(topics = AppConstants.LOCATION_UPDATE_TOPIC, groupId = AppConstants.GROUP_ID)
+
+    public void updatedLocation(String value) {
+        System.out.println("Received location: " + value);
+        
+        
+    }
+
+}
